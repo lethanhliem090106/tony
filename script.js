cc4b6771195ce735dd5cd7f6f07ceff7b23b85de
lethanhliem090106 <liemle2006@gmail.com>
2024-11-28 14:13:22 +0700

    snow project

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function showMessage() {
     document.getElementById('message').style.display = 'block';
-    
 }
 
 function openBearGift(bearNumber) {
     const messages = {
-        1: "Chúc Vy mãi xinh đẹp!",
-        2: "Chúc Vy luôn vui vẻ!",
-        3: "Chúc Vy thành công trong mọi việc!",
-        4: "Chúc Vy luôn khỏe mạnh!",
-        5: "Chúc Vy luôn hạnh phúc!", 
+        1: "Chúc Vy \nmãi xinh đẹp!",
+        2: "Chúc Vy \nluôn vui vẻ!",
+        3: "Chúc Vy \nhọc tập thật tốt!",
+        4: "Chúc Vy \ncó người yêu!",
+        5: "Chúc Vy \nluôn hạnh phúc!", 
     };
 
     const messageElement = document.getElementById(`message${bearNumber}`);
     messageElement.innerText = messages[bearNumber];
-    messageElement.style.display = 'block';
+    messageElement.style.display = 'block'; // Hiển thị thông điệp
 }
-
